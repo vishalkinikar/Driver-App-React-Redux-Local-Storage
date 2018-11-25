ecafdb7c7633c9fcf352a77721e9a23de0291ac4
https://vishalkonline@bitbucket.org <vishalkonline@gmail.com>
2018-11-25 23:29:30 +0530

    gh-pages

---
+++ precache-manifest.01ac9d39021f64f39acd27ee7f5c3a54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f73bfca6f125beb2481b",
+    "url": "/static/js/main.f73bfca6.chunk.js"
+  },
+  {
+    "revision": "c30c0672f6d13eb4544d",
+    "url": "/static/js/1.c30c0672.chunk.js"
+  },
+  {
+    "revision": "f73bfca6f125beb2481b",
+    "url": "/static/css/main.1aa6253b.chunk.css"
+  },
+  {
+    "revision": "c30c0672f6d13eb4544d",
+    "url": "/static/css/1.f6323515.chunk.css"
+  },
+  {
+    "revision": "07e4095c4dbd9e7c5a727a1fdb093cf5",
+    "url": "/index.html"
+  }
+];
